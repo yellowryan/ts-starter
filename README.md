af5a847369bde0821da3f99ec4b80560578fb025
ryan-ziqi <877520264@qq.com>
2023-08-29 00:37:32 +0800

    feat: initial

---
+++ README.md
@@ -0,0 +1,5 @@
+## Usage
+```
+pnpm install -g degit
+degit yellowryan/ts-starter pkg-name
+```
---
+++ test/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, it } from 'vitest';
+
+
+it('should be true', () => {
+  expect(1 + 1).toBe(2)
+})
